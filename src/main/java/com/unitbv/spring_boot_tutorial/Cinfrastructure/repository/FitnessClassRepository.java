51bee470b5c8b980caeb6213d8be819c9d184c14
Flavius Gorgan <gorgan.flavius@ibm.com>
2024-07-04 18:01:10 +0300

    initial config: - pom.xml - application.properties: h2, flyway + schema init - test controller to check tomcat is running

--- src/main/java/com/unitbv/spring_boot_tutorial/Cinfrastructure/repository/FitnessClassRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unitbv.spring_boot_tutorial.Cinfrastructure.repository;
-
-import com.unitbv.spring_boot_tutorial.Ddomain.FitnessClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FitnessClassRepository extends JpaRepository<FitnessClass, String> {
-}
--- src/main/java/com/unitbv/spring_boot_tutorial/Ddomain/exceptions/UnknownObjectException.java
+++
@@ -1,7 +0,0 @@
-package com.unitbv.spring_boot_tutorial.Ddomain.exceptions;
-
-public class UnknownObjectException extends RuntimeException {
-    public UnknownObjectException(String message) {
-        super(message);
-    }
-}
